blackieops/deployawebsite

commit 2b6e30cda43dc60d8f650cf655ac552e9478160a

Make Makefile more incremental
This improves the Makefile to be less of a dumb script and more of a
proper build system, doing incremental builds of only the files that
change (save for the `zod` stuff, which we just trust it to handle on
its own).

Makefile
@@ -1,23 +1,31 @@
-all: build
+SASS = $(shell find assets/sass -type f -name "*.scss")
+CSS = $(SASS:assets/sass/%.scss:dist/assets/site.css)
+IMAGES = $(shell find assets/images -type f)
+DESTIMAGES = $(IMAGES:assets/images/%:dist/assets/images/%)
+
+all: $(CSS) build
 
 dist/assets:
-	mkdir -p dist/assets
+	@mkdir -p dist/assets
 
 assets/sass/components/_syntax.scss: dist/assets
 	bundle exec ruby bin/generate_code_css.rb
 
-build: assets/sass/components/_syntax.scss
+dist/assets/site.css: $(SASS)
 	bundle exec sass \
 		-I vendor/bundle/ruby/*/gems/bourbon-*/app/assets/stylesheets/ \
 		--style compressed \
 		--scss \
 		assets/sass/site.scss dist/assets/site.css
-	cp -r assets/images/ dist/assets/
+
+build: dist/assets/site.css
 	zod site dist
+
+dist/assets/images/%: $(IMAGES)
+	cp -r assets/images/ dist/assets/
 
 clean:
 	rm assets/sass/components/_syntax.scss
 	rm -fr dist/* vendor/bundle .sass-cache
-
 
 .PHONY: all build clean

commit 8c40f0f3839af681d60846bbe2f80fee7142a6e8

Fix and cleanup Makefile
Some regressions were introduced in 2b6e30c, this fixes those.

Additionally, we switch from a phony build target to actually depending
on changes to markdown to recompile the site content.

Also, we stop removing vendor/bundle in the clean target, since rubygems
are not managed by the makefile anymore so it's inappropriate

Makefile
@@ -1,31 +1,34 @@
 SASS = $(shell find assets/sass -type f -name "*.scss")
-CSS = $(SASS:assets/sass/%.scss:dist/assets/site.css)
+CSS = $(SASS:assets/sass/%.scss=dist/assets/site.css)
+
 IMAGES = $(shell find assets/images -type f)
-DESTIMAGES = $(IMAGES:assets/images/%:dist/assets/images/%)
+DESTIMAGES = $(IMAGES:assets/images/%=dist/assets/images/%)
 
-all: $(CSS) build
+MARKDOWNS = $(shell find site/ -type f -name "*.md")
+HTMLS = $(MARKDOWNS:site/%/index.md=dist/%/index.html)
 
-dist/assets:
-	@mkdir -p dist/assets
+all: $(CSS) $(DESTIMAGES) $(HTMLS)
 
-assets/sass/components/_syntax.scss: dist/assets
+assets/sass/components/_syntax.scss:
 	bundle exec ruby bin/generate_code_css.rb
 
 dist/assets/site.css: $(SASS) assets/sass/components/_syntax.scss
+	@mkdir -p dist/assets
 	bundle exec sass \
 		-I vendor/bundle/ruby/*/gems/bourbon-*/app/assets/stylesheets/ \
 		--style compressed \
 		--scss \
 		assets/sass/site.scss dist/assets/site.css
 
-build: dist/assets/site.css
+dist/%/index.html: site/%/index.md
 	zod site dist
 
 dist/assets/images/%: $(IMAGES)
-	cp -r assets/images/ dist/assets/
+	@mkdir -p dist/assets/images
+	cp -v $< $@
 
 clean:
 	rm assets/sass/components/_syntax.scss
-	rm -fr dist/* vendor/bundle .sass-cache
+	rm -fr dist/* .sass-cache
 
-.PHONY: all build clean
+.PHONY: all clean